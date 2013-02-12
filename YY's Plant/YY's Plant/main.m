1719f56bcda79ece8863c7bd368c0d1d6dff0eb3
Gary Xue <garyx.life@gmail.com>
2013-02-12 18:15:43 +0800

    Revert "Revert "有一个初步的界面。一个查询界面。""
    This reverts commit 97a1456b48f15d27184ed87436dc99ce871db228.

---
+++ YY's Plant/YY's Plant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YY's Plant
+//
+//  Created by 薛 洪 on 13-2-12.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GKAppDelegate class]));
+    }
+}
